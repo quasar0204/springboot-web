ca97ed8b743bee955ff61075bd48cd67fa6b8ffa
quasar0204 <quasar13245@gmail.com>
2021-07-13 02:00:00 +0900

    초기 테스트 구현

---
+++ src/main/java/com/quasar/book/springboot/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,26 @@
+package com.quasar.book.springboot.web.dto;
+
+import com.quasar.book.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
