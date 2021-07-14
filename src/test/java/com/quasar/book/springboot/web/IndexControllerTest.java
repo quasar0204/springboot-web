6c2b77dc8a789edd598687ecfead85760a06422f
quasar0204 <quasar13245@gmail.com>
2021-07-14 15:25:55 +0900

    로그인 구현

--- src/test/java/com/quasar/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/quasar/book/springboot/web/IndexControllerTest.java
@@ -22,9 +22,7 @@ public class IndexControllerTest {
         //when
         String body = this.restTemplate.getForObject("/", String.class);
 
-        System.out.println("body asdf = " + body);
-
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스 Ver.2");
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
